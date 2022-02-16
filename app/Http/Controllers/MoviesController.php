6acd9ba3efcae5a9f51f83ab8272feb8f9e4e48c
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-8.local>
2022-02-16 19:26:04 +0100

    movies, view, rute, kontroler, model

---
+++ app/Http/Controllers/MoviesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class MoviesController extends Controller
+{
+    
+        public function index(){
+            $movies = Movie::all();
+            return view('movies',compact('movies'));
+    }
+}
